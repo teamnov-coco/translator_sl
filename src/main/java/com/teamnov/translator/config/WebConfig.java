628c3391cce2ac9612f0d2004a367905dfda1670
teamnov-coco <coco@teamnov.com>
2020-08-24 23:14:32 +0900

    불필요한 소스 정리

--- src/main/java/com/teamnov/translator/config/WebConfig.java
+++ src/main/java/com/teamnov/translator/config/WebConfig.java
@@ -1,23 +1,9 @@
 package com.teamnov.translator.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
-
-// @Configuration
-// public class WebConfig implements WebMvcConfigurer {
-
-//     public void addCorsMappings(CorsRegistry registry) {
-//         registry.addMapping("/**").allowedOrigins("*");
-//     }
-
-// }
 
 @Configuration
 public class WebConfig implements WebFluxConfigurer {
-
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*");
-    }
 
 }
