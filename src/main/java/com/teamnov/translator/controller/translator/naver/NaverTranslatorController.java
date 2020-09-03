5c3670f9516834df031f7649f9e9da534648a4bf
teamnov-coco <coco@teamnov.com>
2020-09-03 17:27:21 +0900

    각 번역기별 컨트롤러 제공

--- src/main/java/com/teamnov/translator/controller/translator/naver/NaverTranslatorController.java
+++ src/main/java/com/teamnov/translator/controller/translator/naver/NaverTranslatorController.java
@@ -27,5 +27,93 @@ public class NaverTranslatorController {
         return naverService.ko2En(txt);
     }
 
+    /**
+     * 한국어 -> 중국어
+     */ 
+    @GetMapping(path = "/ko2cn", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<NovTranslate> ko2Cn(@RequestParam("txt") String txt) {
+        return naverService.ko2Cn(txt);
+    }
+
+    /**
+     * 한국어 -> 일본어
+     */ 
+    @GetMapping(path = "/ko2ja", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<NovTranslate> ko2Ja(@RequestParam("txt") String txt) {
+        return naverService.ko2Ja(txt);
+    }
+
+    /**
+     * 영어 -> 한국어
+     */ 
+    @GetMapping(path = "/en2ko", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<NovTranslate> en2Ko(@RequestParam("txt") String txt) {
+        return naverService.en2Ko(txt);
+    }
+
+    /**
+     * 영어 -> 중국어
+     */ 
+    @GetMapping(path = "/en2cn", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<NovTranslate> en2Cn(@RequestParam("txt") String txt) {
+        return naverService.en2Cn(txt);
+    }
+
+    /**
+     * 영어 -> 일본어
+     */ 
+    @GetMapping(path = "/en2ja", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<NovTranslate> en2Ja(@RequestParam("txt") String txt) {
+        return naverService.en2Ja(txt);
+    }
+
+    /**
+     * 중국어 -> 한국어
+     */ 
+    @GetMapping(path = "/cn2ko", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<NovTranslate> cn2Ko(@RequestParam("txt") String txt) {
+        return naverService.cn2Ko(txt);
+    }
+
+    /**
+     * 중국어 -> 영어
+     */ 
+    @GetMapping(path = "/cn2en", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<NovTranslate> cn2En(@RequestParam("txt") String txt) {
+        return naverService.cn2En(txt);
+    }
+
+    /**
+     * 중국어 -> 일본어
+     */ 
+    @GetMapping(path = "/cn2ja", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<NovTranslate> cn2Ja(@RequestParam("txt") String txt) {
+        return naverService.cn2Ja(txt);
+    }
+
+    /**
+     * 일본어 -> 한국어
+     */ 
+    @GetMapping(path = "/ja2ko", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<NovTranslate> ja2Ko(@RequestParam("txt") String txt) {
+        return naverService.ja2Ko(txt);
+    }
+
+    
+    /**
+     * 일본어 -> 중국어
+     */ 
+    @GetMapping(path = "/ja2cn", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<NovTranslate> ja2Cn(@RequestParam("txt") String txt) {
+        return naverService.ja2Cn(txt);
+    }
+
+    /**
+     * 일본어 -> 영어
+     */ 
+    @GetMapping(path = "/ja2En", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<NovTranslate> ja2En(@RequestParam("txt") String txt) {
+        return naverService.ja2En(txt);
+    }
 
 }
